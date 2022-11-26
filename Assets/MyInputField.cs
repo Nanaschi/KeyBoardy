f9d9e81beeff4fe4cd62eda88a65b54fc33e4e1a
Daniel <naturforscher@outlook.com>
2022-11-26 19:01:58 +0200

    Implemented one placed letters' assignment

--- Assets/MyInputField.cs
+++ Assets/MyInputField.cs
@@ -1,53 +1,41 @@
-
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class MyInputField : InputFieldOriginal
 {
-    public Text myText;        // drag your text object on here
-    private bool shiftOn = false;
-     
+    [SerializeField] private List<UIButton> uiButtons = new();
+
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        foreach (var uiButton in uiButtons)
+        {
+            uiButton.ButtonUI.onClick.AddListener(() => ClickLetter(uiButton.ValueToType));
+        }
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        foreach (var uiButton in uiButtons)
+        {
+            uiButton.ButtonUI.onClick.RemoveAllListeners();
+        }
+        
+    }
+
     public void ClickLetter(string letterClicked)
     {
-        /*DoStateTransition(SelectionState.Selected, true);
-        m_HasDoneFocusTransition = true;*/
+
 
         text += letterClicked;
-        
-        Debug.Log($"{MethodBase.GetCurrentMethod()} {Time.time}", this);
-
-        if(shiftOn) letterClicked = letterClicked.ToUpper();
-        
-        /*myText.text += letterClicked;*/
-        
-        /*OnPointerDown(new PointerEventData(EventSystem.current));
-        SetCaretActive();
-        OnPointerClick(new PointerEventData(EventSystem.current));
-        ActivateInputField();
-        SetCaretActive();
-        m_CaretVisible = true;*/
         m_AllowInput = true;
         caretPosition = text.Length;
         ActivateInputField();
         UpdateLabel();
-        /*MoveTextEnd(false);*/
 
-    }
-     
-    public void ClickBackspace()
-    {
-        string tempGetString = myText.text;
-        if(tempGetString.Length > 0)
-        {
-            string tempString = tempGetString.Substring(0, tempGetString.Length -1);
-            myText.text = tempString;
-        }
-    }
-     
-    public void PressShift()
-    {
-        shiftOn = !shiftOn;
     }
 }
---
+++ Assets/UIButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public struct UIButton
+{
+    [SerializeField] private Button buttonUI;
+    
+    [SerializeField] private string valueToType;
+
+    public Button ButtonUI => buttonUI;
+
+    public string ValueToType => valueToType;
+}
